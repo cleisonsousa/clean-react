4b3becc499b7a8972e36ee84f569dbd773b15a57
Cleison Sousa <cleison.soussa15@gmail.com>
2020-11-14 00:53:30 -0300

    feat: ensure start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,5 +11,12 @@ describe('<Login />', () => {
 
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBeTruthy()
-  })
+
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo Obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo Obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
 })
